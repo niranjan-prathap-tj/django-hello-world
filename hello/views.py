557f4c784b1820df1c217a25e48729fd38d5edb4
niranjan-prathap-tj <niranjan.prathap@ideas2it.com>
2024-07-03 18:24:37 +0530

    test-commit

--- hello/views.py
+++ hello/views.py
@@ -11,7 +11,7 @@ def wish(request):
     return HttpResponse("Good Morning!")
 
 def test(request):
-    return HttpResponse("updated the trigger for ci pipeline")
+    return HttpResponse("Testing the workflow trigger!")
 
 def message(request):
     return HttpResponse("Git hub action change!")
