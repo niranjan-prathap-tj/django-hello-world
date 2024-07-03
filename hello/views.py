ce5bcaae403352c1cd75f8516e2caa407995a54b
niranjan-prathap-tj <niranjan.prathap@ideas2it.com>
2024-07-03 16:55:19 +0530

    updated test message!

--- hello/views.py
+++ hello/views.py
@@ -11,7 +11,7 @@ def wish(request):
     return HttpResponse("Good Morning!")
 
 def test(request):
-    return HttpResponse("tagged version 1.1")
+    return HttpResponse("tagged version v1.1")
 
 def message(request):
     return HttpResponse("Git hub action change!")
