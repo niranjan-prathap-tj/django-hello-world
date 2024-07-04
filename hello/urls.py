b777cf68e63c29525d7323a739ea05a47f4a4faf
niranjan-prathap-tj <niranjan.prathap@ideas2it.com>
2024-07-04 17:14:45 +0530

    updated secret key

--- hello/urls.py
+++ hello/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import hello_world, welcome, wish, test, message
+from .views import hello_world, welcome, wish, test, message, check_db_connection
 
 urlpatterns = [
     path('hello/', hello_world),
     path('',welcome),
     path('wish/',wish),
     path('test/',test),
-    path('message/',message)
+    path('message/',message),
+    path('dbcon/',check_db_connection)
 ]
 
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponse
+from django.db import connection
 
+def check_db_connection(request):
+    connection_details = connection.settings_dict
+    return HttpResponse(f"Database connection details: {connection_details}")
+    
 def hello_world(request):
     return HttpResponse("Hello, world! from Django")
 
