4f09405ff302dcffde9f10fa089cf3701712439c
niranjan-prathap-tj <niranjan.prathap@ideas2it.com>
2024-07-04 12:03:39 +0530

    added gitignore file

--- hello/views.py
+++ hello/views.py
@@ -11,7 +11,7 @@ def wish(request):
     return HttpResponse("Good Morning!")
 
 def test(request):
-    return HttpResponse("Init Test")
+    return HttpResponse("Added git ignore file!")
 
 def message(request):
     return HttpResponse("Git hub action change!")
