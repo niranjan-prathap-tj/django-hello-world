5efb00006653f544ab84f0ca0769f62cc0c64665
niranjan-prathap-tj <niranjan.prathap@ideas2it.com>
2024-07-05 18:17:16 +0530

    ebook - model added!

--- hello/models.py
+++ hello/models.py
@@ -8,3 +8,8 @@ class Book(models.Model):
     publisher = models.CharField(max_length=300, null=True, blank=True)
     def __str__(self):
         return self.title
+
+class ebook(models.Model):
+    title = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
