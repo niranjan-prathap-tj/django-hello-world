76d2b8c13893a683c5fdea34b0476730ba4cd44b
niranjan-prathap-tj <niranjan.prathap@ideas2it.com>
2024-07-05 14:33:17 +0530

    Updated book model

--- hello/models.py
+++ hello/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=300)
     author = models.CharField(max_length=100)
     publication_date = models.DateField()
 
